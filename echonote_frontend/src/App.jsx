c8d98f2cda38a7bc1ff53b303985aebbda8714e3
조민주 <pida6716@gmail.com>
2024-09-24 13:38:28 +0900

    feat: 크기 1280x800 수정(#1)

--- echonote_frontend/src/App.jsx
+++ echonote_frontend/src/App.jsx
@@ -2,33 +2,28 @@ import { Component } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "@shared/styles/GlobalStyles";
 import { theme } from "@shared/styles/theme";
-import { getResizeEventListener } from "@services/responsiveFrame/index";
 import ToolBar from "@components/ToolBar";
 import PdfBar from "@components/PdfBar";
-import { Layout, MainContent } from "@/Layout.style";
+import { Layout, MainContent, rootStyle, appStyle } from "@/Layout.style";
 
 class App extends Component {
   render() {
     return (
-      <div id="App">
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <ToolBar />
-          <Layout>
-            <PdfBar />
-            <MainContent>
-              <p>페이지 내용이 여기에 들어갑니다.</p>
-            </MainContent>
-          </Layout>
-        </ThemeProvider>
+      <div style={rootStyle}>
+        <div style={appStyle}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <ToolBar />
+            <Layout>
+              <PdfBar />
+              <MainContent>
+                <p>페이지 내용이 여기에 들어갑니다.</p>
+              </MainContent>
+            </Layout>
+          </ThemeProvider>
+        </div>
       </div>
     );
-  }
-
-  componentDidMount() {
-    const FixRatio = getResizeEventListener(1280, 800);
-    window.onresize = FixRatio;
-    FixRatio();
   }
 }
 
--- echonote_frontend/src/Layout.style.js
+++ echonote_frontend/src/Layout.style.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 // 레이아웃 컨테이너: 사이드바와 메인 콘텐츠를 배치하는 Flex 레이아웃
 export const Layout = styled.div`
   display: flex;
-  height: calc(100vh - 60px); /* 툴바의 높이를 제외한 나머지 화면 */
-  width: 100%; /* 전체 화면 너비 */
   position: relative; /* 툴바와 독립된 고정 레이아웃 */
-  overflow: hidden; /* 넘치는 콘텐츠 방지 */
+  overflow: hidden;
+  height: 100%;
 `;
 
 // 중앙 메인 콘텐츠를 Flex를 사용해 가운데 정렬
@@ -15,7 +14,20 @@ export const MainContent = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 20px;
   background-color: rebeccapurple;
-  height: 100%; /* 전체 레이아웃 높이를 차지 */
 `;
+
+// 스타일 설정 (1280x800으로 고정)
+export const rootStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  overflow: "hidden", // 화면이 넘치지 않도록 설정
+  backgroundColor: "black", // 검은색 배경
+};
+
+export const appStyle = {
+  width: "1280px", // 고정 너비
+  height: "800px", // 고정 높이
+  backgroundColor: "white", // App 배경색 설정
+};
