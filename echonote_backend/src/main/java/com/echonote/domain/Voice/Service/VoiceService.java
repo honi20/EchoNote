b299e970102571753237ae185680b44975534bcc
이은우 <clairew.tech@gmail.com>
2024-09-24 16:54:55 +0900

    feat : 녹음파일 S3 업로드 API 구현 .wav 확장자를 S3에 저장할 수 있는 presigned-url 발급 Close #5

---
+++ echonote_backend/src/main/java/com/echonote/domain/Voice/Service/VoiceService.java
@@ -0,0 +1,8 @@
+package com.echonote.domain.Voice.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VoiceService {
+
+}
---
+++ echonote_backend/src/main/java/com/echonote/domain/Voice/dao/VoiceRepository.java
@@ -0,0 +1,4 @@
+package com.echonote.domain.Voice.dao;
+
+public interface VoiceRepository {
+}
