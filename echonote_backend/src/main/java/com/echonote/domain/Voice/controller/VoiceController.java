888486623cd330e68d6903390ebd123c3b8ee29c
이은우 <clairew.tech@gmail.com>
2024-09-24 17:13:56 +0900

    style : swagger 수정 swagger 및 url 수정

--- echonote_backend/src/main/java/com/echonote/domain/Voice/controller/VoiceController.java
+++ echonote_backend/src/main/java/com/echonote/domain/Voice/controller/VoiceController.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.HttpMethod;
 import com.echonote.domain.Voice.Service.VoiceServiceImpl;
 import com.echonote.domain.Voice.dto.S3SaveResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/voice")
+@Tag(name = "voice", description = "voice api")
 public class VoiceController {
 	private final VoiceServiceImpl voiceService;
 
@@ -23,7 +28,8 @@ public class VoiceController {
 	private String bucketName;
 
 	// 확장자명에 따라 presigned url 반환
-	@GetMapping("/api/generate-presigned-url")
+	@GetMapping
+	@Operation(summary = "presigned url 반환", description = "클라이언트에서 녹음한 wav 파일 업로드 위한 S3 presigned url 반환")
 	public ResponseEntity<S3SaveResponse> generatePresignedUrl() {
 
 		S3SaveResponse response = voiceService.generatePreSignUrl(UUID.randomUUID() + ".wav", bucketName,
