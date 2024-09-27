5e386d97c1368b6ea96289eb1bc2781910d6b5e7
조민주 <pida6716@gmail.com>
2024-09-27 17:39:32 +0900

    style: 녹음 바 레이아웃을 위한 변경(#11)
    위치나 속성이 바뀌었습니다

--- echonote_frontend/src/components/STTBar.jsx
+++ echonote_frontend/src/components/STTBar.jsx
@@ -1,4 +1,5 @@
 import useSidebarStore from "@stores/sideBarStore";
+import { useState } from "react";
 import {
   STTBarContainer,
   STTBarContent,
@@ -12,13 +13,18 @@ import { FaPen } from "react-icons/fa";
 import SearchBar from "@components/common/SearchBar";
 
 const STTBar = () => {
-  const { isSTTBarOpened, toggleSTTBar } = useSidebarStore();
+  const { isSTTBarOpened } = useSidebarStore();
+  const [isHighLight, setIsHighLight] = useState(false);
+
+  const toggleHighLight = () => {
+    setIsHighLight(!isHighLight);
+  };
 
   return (
     <>
       <STTBarContainer isOpened={isSTTBarOpened}>
         <STTBarHeader>
-          <ToggleSwitch isToggled={isSTTBarOpened} onClick={toggleSTTBar} />
+          <ToggleSwitch isToggled={isHighLight} onClick={toggleHighLight} />
           <IconButton>
             <FaPen />
           </IconButton>
