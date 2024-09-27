4b29b99e4bb9e19986c1125aa6f7ac692c63e5f7
조민주 <pida6716@gmail.com>
2024-09-27 17:38:58 +0900

    feat: 오디오 녹음 기능 추가(#11)

--- echonote_frontend/src/components/RecordingBar.jsx
+++ echonote_frontend/src/components/RecordingBar.jsx
@@ -1,36 +1,13 @@
-import { useState } from "react";
-import { FaPlayCircle, FaPauseCircle, FaStopCircle } from "react-icons/fa";
-import { MdOutlineReplayCircleFilled } from "react-icons/md";
-import {
-  RecordingBarContainer,
-  PlayPauseButton,
-  StopReplayButton,
-} from "@components/styles/RecordingBar.style";
+import useSidebarStore from "@stores/sideBarStore";
+import { RecordingBarContainer } from "@components/styles/RecordingBar.style";
 import AudioWave from "@components/AudioWave";
 
 const RecordingBar = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isRecording, setIsRecording] = useState(false);
-  const audioUrl = "src/assets/test.wav";
-
-  const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleSend = () => {
-    setIsRecording(!isRecording);
-    console.log("Recording sent");
-  };
+  const { isRecordingBarOpened } = useSidebarStore();
 
   return (
-    <RecordingBarContainer>
-      <PlayPauseButton onClick={togglePlayPause}>
-        {isPlaying ? <FaPauseCircle /> : <FaPlayCircle />}
-      </PlayPauseButton>
-      <AudioWave audioUrl={audioUrl} isPlaying={isPlaying} />
-      <StopReplayButton onClick={handleSend}>
-        {isRecording ? <FaStopCircle /> : <MdOutlineReplayCircleFilled />}
-      </StopReplayButton>
+    <RecordingBarContainer isOpened={isRecordingBarOpened}>
+      <AudioWave />
     </RecordingBarContainer>
   );
 };
