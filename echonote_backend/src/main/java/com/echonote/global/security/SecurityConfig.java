2d69571dc36b90a39f48b33bf8e172ffc71eb6b1
최예헌 <1594cyh@gmail.com>
2024-09-30 09:52:55 +0900

    fix : setAllowedOrigins 오류 수정 (#13)

--- echonote_backend/src/main/java/com/echonote/global/security/SecurityConfig.java
+++ echonote_backend/src/main/java/com/echonote/global/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 
 					CorsConfiguration configuration = new CorsConfiguration();
 
-					configuration.setAllowedOrigins(Collections.singletonList(Arrays.asList("http://localhost:3000", "https://REMOVED")));
+					configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://REMOVED"));
 					configuration.setAllowedMethods(Collections.singletonList("*"));
 					configuration.setAllowCredentials(true);
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
