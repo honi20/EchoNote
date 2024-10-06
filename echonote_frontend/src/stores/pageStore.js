e8f0dab60a6fa6acba96786fccd4f0d026232c95
박다솔 <padasol@naver.com>
2024-10-06 23:10:36 +0900

    fix: 페이지 이동 store로 분리 (#8)

--- echonote_frontend/src/stores/pageStore.js
+++ echonote_frontend/src/stores/pageStore.js
@@ -2,8 +2,24 @@ import { create } from "zustand";
 
 const pageStore = create((set, get) => ({
   currentPage: 1, // 현재 페이지 상태 추가
+  pages: 1, // 최대 페이지 수 상태 추가
 
   setCurrentPage: (page) => set(() => ({ currentPage: page })),
+  setPages: (totalPages) => set(() => ({ pages: totalPages })),
+
+  nextPage: () => {
+    const { currentPage, pages } = get();
+    if (currentPage < pages) {
+      set(() => ({ currentPage: currentPage + 1 }));
+    }
+  },
+
+  prevPage: () => {
+    const { currentPage } = get();
+    if (currentPage > 1) {
+      set(() => ({ currentPage: currentPage - 1 }));
+    }
+  },
 }));
 
 export default pageStore;
