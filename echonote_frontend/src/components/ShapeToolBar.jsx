e7adad68830f416d3a13a9b6ddd4d036fc24a025
박다솔 <padasol@naver.com>
2024-10-08 00:31:25 +0900

    feat: 도형 그리기 툴바 생성 (#9)

---
+++ echonote_frontend/src/components/ShapeToolBar.jsx
@@ -0,0 +1,117 @@
+import React, { useState, useRef, useEffect } from "react";
+import * as St from "@components/styles/ShapeToolBar.style";
+import {
+  FaTrash,
+  FaCircle,
+  FaSquare,
+  FaRegCheckCircle,
+  FaRegCircle,
+} from "react-icons/fa";
+import { FaXmark, FaRegCircleXmark } from "react-icons/fa6";
+import { MdOutlineFormatColorFill, MdOutlineLineWeight } from "react-icons/md";
+import shapeStore from "@/stores/shapeStore";
+import drawingTypeStore from "@/stores/drawingTypeStore";
+import { Colorful } from "@uiw/react-color";
+
+const ShapeToolBar = ({}) => {
+  const { property, setFillColor, setStrokeColor } = shapeStore();
+  const { mode, shapeMode, setRectangleMode, setCircleMode } =
+    drawingTypeStore();
+  const [showFillPalette, setShowFillPalette] = useState(false);
+  const [showStrokePalette, setShowStrokePalette] = useState(false);
+  const fillPaletteRef = useRef(null);
+  const strokePaletteRef = useRef(null);
+
+  const toggleFillPalette = () => {
+    if (showStrokePalette) setShowStrokePalette(!showStrokePalette);
+    setShowFillPalette(!showFillPalette);
+  };
+
+  const toggleStrokePalette = () => {
+    if (showFillPalette) setShowFillPalette(!showFillPalette);
+    setShowStrokePalette(!showStrokePalette);
+  };
+
+  const handleRectangleMode = () => {
+    setShowFillPalette(false);
+    setShowStrokePalette(false);
+    setRectangleMode();
+  };
+
+  const handleCircleMode = () => {
+    setShowFillPalette(false);
+    setShowStrokePalette(false);
+    setCircleMode();
+  };
+
+  return (
+    <St.DrawingToolContainer>
+      <St.ToolBarButton>
+        <St.IconContainer>
+          {/* 도형선택 */}
+          <St.IconButton
+            as={FaSquare}
+            isActive={shapeMode.rectangle}
+            onClick={handleRectangleMode}
+          />
+          <St.IconButton
+            as={FaCircle}
+            isActive={shapeMode.circle}
+            onClick={handleCircleMode}
+          />
+        </St.IconContainer>
+        <St.Divider />
+        <St.IconContainer>
+          {/* 채우기색 고르기 */}
+          <St.ColorPaletteContainer>
+            <St.IconButton
+              as={MdOutlineFormatColorFill}
+              onClick={toggleFillPalette}
+              color={property.fillColor}
+            />
+            {showFillPalette && (
+              <St.ColorPalette ref={fillPaletteRef}>
+                <Colorful
+                  color={property.fillColor}
+                  onChange={(newColor) => {
+                    setFillColor(newColor.hexa);
+                  }}
+                />
+                <div>칠 유/무 여부가 들어갑니다</div>
+              </St.ColorPalette>
+            )}
+          </St.ColorPaletteContainer>
+          {/* 선색 고르기 */}
+          <St.ColorPaletteContainer>
+            <St.IconButton
+              as={MdOutlineLineWeight}
+              onClick={toggleStrokePalette}
+              color={property.strokeColor}
+            />
+            {showStrokePalette && (
+              <St.ColorPalette ref={strokePaletteRef}>
+                <Colorful
+                  color={property.strokeColor}
+                  onChange={(newColor) => {
+                    setStrokeColor(newColor.hexa);
+                  }}
+                />
+                <div>선 굵기가 들어갑니다</div>
+                <div>선 유/무 여부가 들어갑니다</div>
+              </St.ColorPalette>
+            )}
+          </St.ColorPaletteContainer>
+        </St.IconContainer>
+        {/* <ColorPalette value={property.fillColor} onChange={setFillColor} /> */}
+        <St.Divider />
+        <St.IconContainer>
+          {/* 도형 지우기, toolbar 닫기 */}
+          <St.IconButton as={FaTrash} />
+          <St.IconButton as={FaRegCircleXmark} />
+        </St.IconContainer>
+      </St.ToolBarButton>
+    </St.DrawingToolContainer>
+  );
+};
+
+export default ShapeToolBar;
---
+++ echonote_frontend/src/components/styles/ShapeToolBar.style.js
@@ -0,0 +1,166 @@
+import { styled } from "styled-components";
+import { theme } from "@/shared/styles/theme";
+import { shouldNotForwardPropsWithKeys } from "@shared/utils/shouldForwardProp";
+
+export const DrawingToolContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 30px;
+  max-width: 350px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(65, 65, 65, 0.1);
+  margin: 0 auto;
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+`;
+export const ToolBarButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+`;
+
+export const IconButton = styled.div.withConfig({
+  shouldForwardProp: (prop) => !["isActive"].includes(prop), // isActive만 필터링
+})`
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.colors.selectedIconBackground : "transparent"};
+  border: none;
+  border-radius: 15px;
+  color: ${(color) => (color ? color : theme.colors.iconColor)};
+  cursor: pointer;
+  font-size: 18px;
+  padding: 6px;
+`;
+
+export const IconIndex = styled.div`
+  background-color: transparent;
+  color: ${theme.colors.iconColor};
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+`;
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 24px;
+  background-color: #d3d3d3;
+  margin: 0 10px;
+`;
+
+// 공통 슬라이더 스타일 적용
+const CommonSliderPopup = styled.div`
+  position: absolute;
+  z-index: 9999;
+  bottom: 60px;
+  background-color: white;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  label {
+    margin-bottom: 5px;
+  }
+
+  input[type="range"] {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 150px;
+    height: 6px;
+    background: #ddd;
+    outline: none;
+    opacity: 0.7;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background: black;
+      cursor: pointer;
+      margin-top: -5px;
+      transition: background 0.2s;
+    }
+
+    &::-moz-range-thumb {
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background: black;
+      cursor: pointer;
+      transition: background 0.2s;
+    }
+
+    &::-webkit-slider-runnable-track {
+      background: linear-gradient(black, black) no-repeat center;
+      height: 6px;
+      border-radius: 3px;
+    }
+  }
+`;
+
+//폰트 사이즈, 도형 색상 열고 닫는용
+export const ToolBarIconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// 폰트, 도형 애니메이션
+export const ToolBarIconDetail = styled.div.withConfig({
+  shouldForwardProp: (prop) => !["isOpen"].includes(prop), // isOpen만 필터링
+})`
+  max-width: ${(props) => (props.isOpen ? "80px" : "0")};
+  opacity: ${(props) => (props.isOpen ? "1" : "0")};
+  transform-origin: left;
+  transition: max-width 0.5s ease, opacity 0.5s ease, visibility 0.5s ease;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  margin-left: ${(props) => (props.isOpen ? "5px" : "0")};
+`;
+
+export const ColorPalette = styled.div`
+  position: absolute;
+  z-index: 2;
+  bottom: 70px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 12px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const ColorPaletteContainer = styled.div`
+  position: relative;
+  display: inline-block;
+`;
