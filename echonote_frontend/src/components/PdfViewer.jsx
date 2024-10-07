5bcbffe41e4a21f716271e81db7d9c26ac00d6c7
박다솔 <padasol@naver.com>
2024-10-08 00:32:53 +0900

    feat: 임시 속성 선택 컴포넌트 제거 후 도형 툴바 추가 (#9)

--- echonote_frontend/src/components/PdfViewer.jsx
+++ echonote_frontend/src/components/PdfViewer.jsx
@@ -1,15 +1,17 @@
 import { useRef } from "react";
 import PdfCanvas from "@components/PdfCanvas";
 import * as St from "@components/styles/PdfViewer.style";
-import PropertyEditor from "@/components/TmpShapeProperty";
+import ShapeToolBar from "./ShapeToolBar";
+import drawingTypeStore from "@/stores/drawingTypeStore";
 
 const PdfViewer = ({ isDrawingEditorOpened, savedImage }) => {
   const containerRef = useRef();
+  const { mode } = drawingTypeStore();
 
   return (
     <St.PdfContainer ref={containerRef}>
       <St.ButtonContainer>
-        <PropertyEditor />
+        {mode.shape ? <ShapeToolBar /> : null}
       </St.ButtonContainer>
       <PdfCanvas
         containerRef={containerRef}
