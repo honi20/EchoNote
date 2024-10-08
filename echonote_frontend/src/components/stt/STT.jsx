c803e107b5e4b26fa4e38e44b1625a6b6c3ba38d
이현주 <liveinmars2018@gmail.com>
2024-10-08 19:14:08 +0900

    feat: sse 연결 코드 추가

--- echonote_frontend/src/components/stt/STT.jsx
+++ echonote_frontend/src/components/stt/STT.jsx
@@ -49,14 +49,42 @@ const STTComponent = ({ id, searchTerm, isEditMode, onSubmit }) => {
 
 // 컴포넌트 마운트 시 API 데이터 가져오기
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getSTTResult(id);
-      if (data && data.result) {
-        setSttData(data.result);
-      }
+    const eventSource = new EventSource(' http://localhost:8080/voice/sse?note_id=1');
+
+    console.log("SSE 연결 시도 중...");
+
+    // 연결 시 초기 메시지 처리
+    eventSource.onopen = (event) => {
+      console.log("연결 완료: ", event);
     };
-    fetchData();
-  }, [id]);
+
+    // STT 완료 이벤트 처리
+    eventSource.addEventListener('stt_complete', (event) => {
+      console.log("STT 완료: ", event.data);
+      setEventMessage('STT 정보 수신 완료');
+
+      alert("STT 완료!");
+    });
+
+    // 일반 메시지 처리
+    eventSource.onmessage = (event) => {
+      console.log("메시지 수신: ", event.data);
+    };
+
+    // 오류 처리
+    eventSource.onerror = (event) => {
+      console.error("SSE 오류 발생:", event);
+      console.error("readyState:", eventSource.readyState); // 상태 로그
+      eventSource.close();  // 연결 종료
+    };
+
+    // 컴포넌트 언마운트 시 SSE 연결 닫기
+    return () => {
+      eventSource.close();
+      console.log("SSE 연결 종료");
+    };
+  }, []);
+
 
 
 
