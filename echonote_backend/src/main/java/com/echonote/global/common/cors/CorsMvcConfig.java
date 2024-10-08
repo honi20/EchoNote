8db4127ff5a68f6d34c6af03a05ea5c9ffa72d5e
이현주 <liveinmars2018@gmail.com>
2024-10-08 12:04:35 +0900

    feat: SSE 코드 설정

--- echonote_backend/src/main/java/com/echonote/global/common/cors/CorsMvcConfig.java
+++ echonote_backend/src/main/java/com/echonote/global/common/cors/CorsMvcConfig.java
@@ -16,7 +16,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 				"OPTIONS") // 허용하려는 HTTP Method 설정 (OPTIONS는 Preflight 설정)
 			.allowedOriginPatterns("http://localhost:3000", "https://REMOVED/api", "http://localhost:5173") // 허용하려는 클라이언트 측 주소
 			.allowCredentials(true) // HttpOnly Cookie를 사용하기 위한 설정
-			.allowedHeaders("Content-Type", "Authorization", "X-Requested-With") // 허용할 헤더에 Content-Type 추가
+			.allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin", "text/event-stream") // 허용할 헤더에 Content-Type 추가
 			.exposedHeaders("Set-Cookie")
 			.exposedHeaders("Access-Control-Allow-Methods")
 			.exposedHeaders("Access-Control-Allow-Headers")
