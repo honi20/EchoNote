54055c859f6fc7ce99979506cec2a71588f4cf90
박다솔 <padasol@naver.com>
2024-10-08 13:28:18 +0900

    feat: 녹음 시작 시각 기록 추가 (#21)

--- echonote_frontend/src/stores/recordStore.js
+++ echonote_frontend/src/stores/recordStore.js
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 
 export const useAudioStore = create((set) => ({
+  createTime: null,
   startTime: null,
   setStartTime: (time) => set({ startTime: time }),
+  setCreatetime: (time) => set({ createTime: time }),
 }));
