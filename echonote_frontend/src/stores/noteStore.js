950d45f05064b539b3c14fc231e83c12391f4b94
조민주 <pida6716@gmail.com>
2024-10-10 01:12:24 +0900

    feat: sse 코드 및 noteId와 녹음 연결(#24)

--- echonote_frontend/src/stores/noteStore.js
+++ echonote_frontend/src/stores/noteStore.js
@@ -1,17 +1,19 @@
 import { create } from "zustand";
 
 export const useNoteStore = create((set) => ({
-  note_id: null,
+  note_id: 0,
   note_name: null,
   pdf_path: null,
   record_path: null,
+  stt_status: "processing",
 
   // 상태 업데이트 함수
   setNoteDetail: (note) =>
     set({
-      note_id: note.note_id,
+      note_id: Number(note.note_id),
       note_name: note.note_name,
       pdf_path: note.pdf_path,
       record_path: note.record_path,
+      stt_status: note.stt_status,
     }),
 }));
