71e7c3a95d04751876ab19feafb4756b1c695774
박다솔 <padasol@naver.com>
2024-10-10 10:43:24 +0900

    style: theme으로 변경 (#8)

--- echonote_frontend/src/components/common/LoadingIcon.jsx
+++ echonote_frontend/src/components/common/LoadingIcon.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { MoonLoader } from "react-spinners";
 import * as St from "@components/common/LoadingIcon.style";
+import { theme } from "@/shared/styles/theme";
 
 const LoadingIcon = () => {
   return (
     <St.LoadingIconContainer>
       <St.LoadingText>PDF를 가져오고 있어요</St.LoadingText>
-      <MoonLoader color="#FF900D" />
+      <MoonLoader color={theme.colors.main} />
     </St.LoadingIconContainer>
   );
 };
